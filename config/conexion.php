1e01fc2fab9a98e7df319554acb4a1e4cd1fd354
Ronald <ragcorp.g@outlook.com>
2021-05-30 15:34:02 -0600

    chan

--- config/conexion.php
+++ config/conexion.php
@@ -1,18 +1,18 @@
 <?php
 	
-	# conectare la base de datos
-    $con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-    if(!$con){
-        die("imposible conectarse: ".mysqli_error($con));
-    }
-    if (@mysqli_connect_errno()) {
-        die("Conexión falló: ".mysqli_connect_errno()." : ". mysqli_connect_error());
-    }
+	// # conectare la base de datos
+    // $con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    // if(!$con){
+    //     die("imposible conectarse: ".mysqli_error($con));
+    // }
+    // if (@mysqli_connect_errno()) {
+    //     die("Conexión falló: ".mysqli_connect_errno()." : ". mysqli_connect_error());
+    // }
 ?>
 
 
 /<?php
-/* $serverName = "RAG-5\SQLEXPRESS"; //serverName\instanceName
+ $serverName = "RAG-5\SQLEXPRESS"; //serverName\instanceName
 
 // Puesto que no se han especificado UID ni PWD en el array  $connectionInfo,
 // La conexión se intentará utilizando la autenticación Windows.
@@ -24,5 +24,5 @@ if( $con ) {
 }else{
      echo "Conexión no se pudo establecer.<br />";
      die( print_r( sqlsrv_errors(), true));
-} */
-// ?>
+} 
+ ?> */
