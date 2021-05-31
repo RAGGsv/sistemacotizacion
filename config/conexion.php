da957bd6be6498e5ac39e47299d803940d8eb44c
Ronald <ragcorp.g@outlook.com>
2021-05-30 20:24:59 -0600

    conexion

--- config/conexion.php
+++ config/conexion.php
@@ -1,15 +1,3 @@
-<?php
-	
-	// # conectare la base de datos
-    // $con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-    // if(!$con){
-    //     die("imposible conectarse: ".mysqli_error($con));
-    // }
-    // if (@mysqli_connect_errno()) {
-    //     die("Conexión falló: ".mysqli_connect_errno()." : ". mysqli_connect_error());
-    // }
-?>
-
 
 <?php
  $serverName = "RAG-5\SQLEXPRESS"; //serverName\instanceName
